fa9096b849d29600de8b6ec1be2da53e212e4213
Roman Krof <krof.roman.2018@skola.ssps.cz>
2021-03-07 17:30:47 +0100

    Přidejte soubory projektu.

---
+++ Shopping List - Krof/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Shopping_List___Krof
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plus = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(40, 70);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(20, 20);
+            this.plus.TabIndex = 0;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nakup si";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hotovo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 575);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Shopping List - Krof/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping_List___Krof
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static List<Panel> list = new List<Panel>();
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            Panel polozka = new Panel() { Size = new Size(720, 20), Location = new Point(10, 62) };
+
+            TextBox text = new TextBox() { Parent = polozka, Size = new Size(345, 20), Location = new Point(85, 0) };
+
+            CheckBox check = new CheckBox() { Parent = polozka, Size = new Size(15, 20), Location = new Point(531, 0) };
+
+            Button del = new Button() { Parent = polozka, Size = new Size(83, 20), Location = new Point(633, 0), Text = "Smazat" };
+
+            del.Click += new System.EventHandler(this.DeleteButton_Click);
+
+            list.Add(polozka);
+            Controls.Add(polozka);
+
+            plus.Top += 30;
+
+            Seradit();
+        }
+
+        private void Seradit()
+        {
+            foreach (Control pan in this.Controls)
+            {
+                if (pan.GetType().Name == "Panel")
+                {
+                    pan.Location = new Point(10, 70 + list.IndexOf((Panel)pan) * 30);
+                }                                  
+            }         
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            Button but = (Button)sender;
+            Controls.Remove((Panel)but.Parent);
+            list.Remove((Panel)but.Parent);
+
+            plus.Top -= 30;
+
+            Seradit();
+        }      
+    }
+}
